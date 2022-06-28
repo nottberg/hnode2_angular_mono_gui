1ec3c563bb4a263ed9fb70a3a831d317a022acaf
Curtis Nottberg <curtisn@comcast.net>
2022-06-27 21:27:08 -0600

    Initial partial implementation of sequence and sequence-edit components.

--- src/app/hnid-sequence-edit-dialog/hnid-sequence-edit-dialog.component.ts
+++ src/app/hnid-sequence-edit-dialog/hnid-sequence-edit-dialog.component.ts
@@ -1,4 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog'
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { Sequence, NMObjSelectionTracker } from '../_services/irrigation-data.service';
+
+// Bitfield values for tracking updates
+export const SQUPD_NONE        = 0;  // 0b00000000
+export const SQUPD_NAME        = 1;  // 0b00000001
+export const SQUPD_DESC        = 2;  // 0b00000010
+export const SQUPD_TYPE        = 4;  // 0b00000100
+export const SQUPD_ONDURATION  = 8;  // 0b00001000
+export const SQUPD_OFFDURATION = 16; // 0b00010000
+export const SQUPD_OBJIDLIST   = 32; // 0b00100000
 
 @Component({
   selector: 'app-hnid-sequence-edit-dialog',
@@ -6,10 +18,73 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hnid-sequence-edit-dialog.component.scss']
 })
 export class HnidSequenceEditDialogComponent implements OnInit {
+  form!: FormGroup;
+  description: string;
 
-  constructor() { }
+  curSeq: Sequence;
+
+  availZones : NMObjSelectionTracker[];
+
+  updateFlags: number;
+
+  constructor(
+    private fb: FormBuilder,
+    private dialogRef: MatDialogRef<HnidSequenceEditDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) data : any) { 
+
+    this.description = data.description;
+    this.updateFlags = SQUPD_NONE;
+
+    if( data.curSeq )
+      this.curSeq = data.curSeq;
+    else
+      this.curSeq = {sequenceid: "", name: "", description: "", type: "", onDuration: 0, offDuration: 0, objIDList: [] };
+
+     this.availZones = data.availZones;
+
+     console.log(data);
+  }
 
   ngOnInit(): void {
+    this.form = this.fb.group({
+      description: [this.description, []],
+      nameFC: [this.curSeq.name, []],
+      descriptionFC: [this.curSeq.description, []],      
+      onDurationFC: [this.curSeq.onDuration, []],
+      offDurationFC: [this.curSeq.offDuration, []]
+    });
+  }
+
+  save() {
+  //  let rtnDayArr : string[] = [];
+  //  if( this.dayList[0].selected == false )
+  //  {
+  //    for( let i = 0; i < this.dayList.length; i++ )
+  //      if( this.dayList[i].selected == true )
+  //        rtnDayArr.push( this.dayList[i].name );
+  //  }
+
+    const rtnData = { form: this.form.value,
+                      updFlags: this.updateFlags
+                      //dayArr: rtnDayArr,
+                      };
+
+    this.dialogRef.close(rtnData);
+  }
+
+  close() {
+    this.dialogRef.close();
+  }
+
+  onFieldUpdate( ctrlID: string ) {
+    if( ctrlID == "nameFC" )
+      this.updateFlags |= SQUPD_NAME;
+    else if( ctrlID == "descriptionFC" )
+      this.updateFlags |= SQUPD_DESC;
+    else if( ctrlID == "onDurationFC" )
+      this.updateFlags |= SQUPD_ONDURATION;
+    else if( ctrlID == "offDurationFC" )
+      this.updateFlags |= SQUPD_OFFDURATION; 
   }
 
 }
--- src/app/hnid-sequences/hnid-sequences.component.ts
+++ src/app/hnid-sequences/hnid-sequences.component.ts
@@ -1,15 +1,221 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { SelectionModel } from '@angular/cdk/collections';
+import { MatTableDataSource } from '@angular/material/table';
+import { HnidSequenceEditDialogComponent, SQUPD_NAME, SQUPD_DESC, SQUPD_TYPE, SQUPD_ONDURATION, SQUPD_OFFDURATION, SQUPD_OBJIDLIST } from '../hnid-sequence-edit-dialog/hnid-sequence-edit-dialog.component';
+import { HnidConfirmDialogComponent } from '../hnid-confirm-dialog/hnid-confirm-dialog.component';
+import { IrrigationDataService, NamedObj, Sequence } from '../_services/irrigation-data.service';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-hnid-sequences',
   templateUrl: './hnid-sequences.component.html',
-  styleUrls: ['./hnid-sequences.component.scss']
+  styleUrls: ['./hnid-sequences.component.scss'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ]
 })
 export class HnidSequencesComponent implements OnInit {
+  crc32ID : string | null;
+  errMsg : string;
 
-  constructor() { }
+  znmList: NamedObj[] = [];
+  sequencesList: Sequence[] = [];
+
+  dataSource = new MatTableDataSource<Sequence>( this.sequencesList );
+  selection = new SelectionModel<Sequence>( false, [] );
+
+  columnsToDisplay : string[] = ['select', 'nameCol', 'type', 'onDuration', 'offDuration'];
+  columnsToDisplayWithExpand = [...this.columnsToDisplay, 'expand'];
+  expandedElement: Sequence | null = null;
+
+  constructor( private route: ActivatedRoute, private irrData: IrrigationDataService, private dialog: MatDialog ) {
+    this.crc32ID = null;
+    this.errMsg = "";  
+  }
+
+  refreshSequencesConfig() : void {
+    this.selection.clear();
+    const tmpID: string = this.crc32ID !== null ? this.crc32ID : '';
+    this.irrData.getSequencesConfig( tmpID ).subscribe({
+      next: data => {
+        this.sequencesList = data.sequencesList;
+        this.znmList = data.znmList;
+        this.dataSource.data = this.sequencesList;
+        console.log( this.sequencesList );
+        console.log( this.znmList );       
+      },
+      error: err => {
+        this.sequencesList = [];
+        this.znmList = [];
+        this.dataSource.data = this.sequencesList;
+        this.errMsg = JSON.parse(err.error).message;
+      }
+    });
+  }
 
   ngOnInit(): void {
+    
+    this.route.paramMap.subscribe((params: ParamMap) => {
+      this.crc32ID = params.get('crc32ID');
+      this.refreshSequencesConfig();
+    });
+
+  }
+
+  getObjectName( zoneid: string ) : string {
+    for( let i = 0; i < this.znmList.length; i++ )
+    {
+      if( this.znmList[i].id == zoneid )
+        return this.znmList[i].name;
+    }
+
+    return "Zone Not Found (" + zoneid + ")";
+  }
+
+  onEditButtonClick(): void
+  {
+    const dialogCfg = new MatDialogConfig();
+
+    dialogCfg.autoFocus = true;
+
+    console.log(this.selection);
+    
+    if( this.selection.selected.length != 1 )
+      return;
+
+    let curSeq : Sequence = this.selection.selected[0];
+
+    dialogCfg.data = {
+      description: 'Edit Sequence',
+      curSeq: curSeq,
+      availZones: this.znmList
+    };
+
+    const dialogRef = this.dialog.open( HnidSequenceEditDialogComponent, dialogCfg );
+
+    dialogRef.afterClosed().subscribe(
+      data => {
+        if( data )
+        {
+          var updateFields: Record<string,any> = {sequenceID: curSeq.sequenceid};
+          console.log("Dialog output:", data);
+          
+          if( data.updFlags & SQUPD_NAME )
+            updateFields['name'] = data.form.nameFC;
+          if( data.updFlags & SQUPD_DESC )
+            updateFields['description'] = data.form.descriptionFC;
+          if( data.updFlags & SQUPD_TYPE )
+            updateFields['type'] = data.form.typeFC;
+          if( data.updFlags & SQUPD_ONDURATION )
+            updateFields['onDuration'] = data.form.onDurationFC;
+          if( data.updFlags & SQUPD_OFFDURATION )
+            updateFields['offDuration'] = data.form.offDurationFC;
+          if( data.updFlags & SQUPD_OBJIDLIST )
+            updateFields['objIDList'] = data.objIDList;
+          
+          console.log( updateFields );
+
+          const tmpID: string = this.crc32ID !== null ? this.crc32ID : '';
+          this.irrData.putUpdateSequence( tmpID, curSeq.sequenceid, updateFields ).subscribe(resp=>{
+            console.log('Sequence Updated');
+            this.refreshSequencesConfig();
+          });
+        }
+        else
+          console.log("dialog Canceled");
+        }
+    );
+  }
+
+  onNewButtonClick(): void
+  {
+    const dialogCfg = new MatDialogConfig();
+
+    dialogCfg.autoFocus = true;
+
+    dialogCfg.data = {
+      description: 'Create Sequence',
+      curSeq: null,
+      availZones: this.znmList
+    };
+
+    const dialogRef = this.dialog.open( HnidSequenceEditDialogComponent, dialogCfg );
+
+    dialogRef.afterClosed().subscribe(
+      data => {
+        if( data )
+        {
+          var updateFields: Record<string,any> = {};
+          console.log("Dialog output:", data);
+          
+          if( data.updFlags & SQUPD_NAME )
+            updateFields['name'] = data.form.nameFC;
+          if( data.updFlags & SQUPD_DESC )
+            updateFields['description'] = data.form.descriptionFC;
+          if( data.updFlags & SQUPD_TYPE )
+            updateFields['type'] = data.form.typeFC;
+          if( data.updFlags & SQUPD_ONDURATION )
+            updateFields['onDuration'] = data.form.onDurationFC;
+          if( data.updFlags & SQUPD_OFFDURATION )
+            updateFields['offDuration'] = data.form.offDurationFC;
+          if( data.updFlags & SQUPD_OBJIDLIST )
+            updateFields['objIDList'] = data.objIDList;
+
+          console.log( updateFields );
+
+          const tmpID: string = this.crc32ID !== null ? this.crc32ID : '';
+          this.irrData.postCreateSequence( tmpID, updateFields ).subscribe(resp=>{
+            console.log('Sequence Created');
+            this.refreshSequencesConfig();
+          });
+        }
+        else
+          console.log("dialog Canceled");
+        }
+    );
+
+  }
+
+  onDeleteButtonClick(): void
+  {
+    const dialogCfg = new MatDialogConfig();
+
+    dialogCfg.autoFocus = true;
+
+    console.log(this.selection);
+    
+    if( this.selection.selected.length != 1 )
+      return;
+
+    let curSeq : Sequence = this.selection.selected[0];
+
+    dialogCfg.data = {
+      prompt: 'Delete placement - ' + curSeq.name
+    };
+
+    const dialogRef = this.dialog.open( HnidConfirmDialogComponent, dialogCfg );
+
+    dialogRef.afterClosed().subscribe(
+      data => {
+        if( data )
+        {
+          console.log("Delete confirmed");
+          const tmpID: string = this.crc32ID !== null ? this.crc32ID : '';
+          this.irrData.deleteSequence( tmpID, curSeq.sequenceid ).subscribe(resp=>{
+            console.log('Sequence Deleted');
+            this.refreshSequencesConfig();
+          });
+        }
+        else
+          console.log("dialog Canceled");
+        }
+    );
   }
 
 }
